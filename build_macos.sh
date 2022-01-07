3d992bb398c2b3e203c39259b7923d701724e216
David Braun <2096055+DBraun@users.noreply.github.com>
2022-01-06 20:22:22 -0800

    might fix x86_64 and M1 cross support on Github Actions

--- build_macos.sh
+++ build_macos.sh
@@ -1,8 +1,7 @@
 # Build Libsamplerate
 cd thirdparty/libsamplerate
 mkdir build_release
-# todo: need to do this for both x86_64 and arm64
-cmake -DCMAKE_BUILD_TYPE=Release -Bbuild_release -DCMAKE_OSX_ARCHITECTURES="arm64"
+cmake -DCMAKE_BUILD_TYPE=Release -Bbuild_release -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" -DLIBSAMPLERATE_EXAMPLES=off
 make --directory=build_release
 cd ../..
 
