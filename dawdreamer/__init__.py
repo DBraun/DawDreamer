57b3cd4499a6297a1a1ae511e6afbd3a58f3faf4
David Braun <2096055+DBraun@users.noreply.github.com>
2022-01-10 15:19:26 -0800

    maybe this is the better way to import dawdreamer

--- dawdreamer/__init__.py
+++ dawdreamer/__init__.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
-from .dawdreamer import *
+import importlib
+# https://stackoverflow.com/a/43059528
+# get a handle on the module
+mdl = importlib.import_module('.dawdreamer', '.dawdreamer')
+
+# is there an __all__?  if so respect it
+if "__all__" in mdl.__dict__:
+    names = mdl.__dict__["__all__"]
+else:
+    # otherwise we import all names that don't begin with _
+    names = [x for x in mdl.__dict__ if not x.startswith("_")]
+
+# now drag them in
+globals().update({k: getattr(mdl, k) for k in names})
