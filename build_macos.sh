780b4c524d3d2d1710d415a6876d0ce8ca1cab12
David Braun <2096055+DBraun@users.noreply.github.com>
2022-01-11 13:23:29 -0800

    try to fix macos test

--- build_macos.sh
+++ build_macos.sh
@@ -13,7 +13,7 @@ xcodebuild -configuration Release -project Builds/MacOSX/DawDreamer.xcodeproj/
 mv Builds/MacOSX/build/Release/dawdreamer.so.dylib Builds/MacOSX/build/Release/dawdreamer.so
 # otool -L Builds/MacOSX/build/Release/dawdreamer.so
 install_name_tool -change @rpath/libfaust.2.dylib @loader_path/libfaust.2.dylib Builds/MacOSX/build/Release/dawdreamer.so
-install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.9/Python intentionally_missing_python.dylib Builds/MacOSX/build/Release/dawdreamer.so
+install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.9/Python @executable_path/../../../../Python Builds/MacOSX/build/Release/dawdreamer.so
 # otool -L Builds/MacOSX/build/Release/dawdreamer.so
 
 # codesigning
@@ -31,6 +31,6 @@ cp Builds/MacOSX/build/Release/dawdreamer.so tests/dawdreamer.so
 # pip install setuptools wheel build delocate
 # python3 -m build --wheel
 # delocate-listdeps dist/dawdreamer-0.5.8.1-cp39-cp39-macosx_10_15_universal2.whl 
-# delocate-wheel --ignore-missing-dependencies --require-archs x86_64 -w repaired_wheel.whl dist/dawdreamer-0.5.8.1-cp39-cp39-macosx_10_15_universal2.whl
+# delocate-wheel --require-archs x86_64 -w repaired_wheel dist/dawdreamer-0.5.8.1-cp39-cp39-macosx_10_15_universal2.whl
 # pip install repaired_wheel/dawdreamer-0.5.8.1-cp39-cp39-macosx_10_15_universal2.whl
 # cd tests && python -m pytest .
