ca82eb6c978d1a79e2ce06846944354280d3a4c7
David Braun <2096055+DBraun@users.noreply.github.com>
2022-01-11 11:25:41 -0800

    try to fix macos python dependency

--- build_macos.sh
+++ build_macos.sh
@@ -7,11 +7,13 @@ cd ../..
 
 # build macOS release
 export PYTHONMAJOR=3.9
+export MACOSX_DEPLOYMENT_TARGET=10.15
 export pythonLocation=/Library/Frameworks/Python.framework/Versions/3.9
 xcodebuild -configuration Release -project Builds/MacOSX/DawDreamer.xcodeproj/
 mv Builds/MacOSX/build/Release/dawdreamer.so.dylib Builds/MacOSX/build/Release/dawdreamer.so
 # otool -L Builds/MacOSX/build/Release/dawdreamer.so
 install_name_tool -change @rpath/libfaust.2.dylib @loader_path/libfaust.2.dylib Builds/MacOSX/build/Release/dawdreamer.so
+install_name_tool -change /Library/Frameworks/Python.framework/Versions/3.9/Python intentionally_missing_python.dylib Builds/MacOSX/build/Release/dawdreamer.so
 # otool -L Builds/MacOSX/build/Release/dawdreamer.so
 
 # codesigning
@@ -26,6 +28,9 @@ rm tests/dawdreamer.so
 cp Builds/MacOSX/build/Release/dawdreamer.so tests/dawdreamer.so
 
 # # To make a wheel locally:
-# export MACOSX_DEPLOYMENT_TARGET=10.15
-# pip install setuptools wheel build
+# pip install setuptools wheel build delocate
 # python3 -m build --wheel
+# delocate-listdeps dist/dawdreamer-0.5.8.1-cp39-cp39-macosx_10_15_universal2.whl 
+# delocate-wheel --ignore-missing-dependencies --require-archs x86_64 -w repaired_wheel.whl dist/dawdreamer-0.5.8.1-cp39-cp39-macosx_10_15_universal2.whl
+# pip install repaired_wheel/dawdreamer-0.5.8.1-cp39-cp39-macosx_10_15_universal2.whl
+# cd tests && python -m pytest .
--- dawdreamer/__init__.py
+++ dawdreamer/__init__.py
@@ -1,8 +1,4 @@
-
-import os.path
-import glob
-
-contents = glob.glob(os.path.join(os.path.dirname(__file__), '*'))
-print('contents: ', contents)
+# NB: on cibuildwheel, this __init__.py file ends up at roughly
+# /private/var/folders/24/8k48jl6d249_n_qfxwsl6xvm0000gn/T/tmp6qlby_wi/lib/python3.8/site-packages/dawdreamer/__init__.py
 
 from .dawdreamer import *
