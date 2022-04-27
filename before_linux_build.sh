1fe126f78e301f93c23b717de0819e24c581e7c2
David Braun <email@example.com>
2022-04-27 10:51:59 -0700

    .

--- before_linux_build.sh
+++ before_linux_build.sh
@@ -1,4 +1,7 @@
-echo "pythonLocation: " $pythonLocation
+echo "which python"
+which python
+# pythonLocation=$(python3-config --prefix)
+# echo "pythonLocation: " $pythonLocation
 yum install -y libsndfile \
 libX11-devel \
 libXrandr-devel \
@@ -27,7 +30,7 @@ cd ../../..
 cd Builds/LinuxMakefile
 ldconfig
 PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig/:/usr/local/include/freetype2/freetype/config:../../alsa-lib/utils:/__w/DawDreamer/DawDreamer/alsa-lib/utils
-make CONFIG=Release LIBS="-lstdc++fs" LDFLAGS="-L/__w/DawDreamer/DawDreamer/alsa-lib/src" CXXFLAGS="-I../../alsa-lib/include -I../../curl/include -I../../freetype/include"
+make VERBOSE=1 CONFIG=Release LIBS="-lstdc++fs" LDFLAGS="-L/__w/DawDreamer/DawDreamer/alsa-lib/src" CXXFLAGS="-I../../alsa-lib/include -I../../curl/include -I../../freetype/include"
 mv build/libdawdreamer.so ../../dawdreamer/dawdreamer.so
 cd ../..
 cp thirdparty/libfaust/ubuntu-x86_64/lib/libfaust.so dawdreamer/libfaust.so
@@ -36,3 +39,4 @@ rm -f thirdparty/libfaust/ubuntu-x86_64/lib/libfaust.so
 rm -f thirdparty/libfaust/ubuntu-aarch64/lib/libfaust.so
 rm -f thirdparty/faust/architecture/android/app/lib/libsndfile/lib/armeabi-v7a/libsndfile.so
 rm -f thirdparty/faust/architecture/android/app/lib/libsndfile/lib/arm64-v8a/libsndfile.so
+echo "before_linux_build.sh is done!"
