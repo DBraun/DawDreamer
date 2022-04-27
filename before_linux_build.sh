886c42a7dc8d79697fb02ae03d6398064b83f5b8
David Braun <email@example.com>
2022-04-27 12:43:15 -0700

    sh parsing

--- before_linux_build.sh
+++ before_linux_build.sh
@@ -1,3 +1,9 @@
+PYTHONMAJOR = "$1"
+PYTHONVERSION = "$2"
+
+echo "PYTHONMAJOR: $PYTHONMAJOR" 
+echo "PYTHONVERSION: $PYTHONVERSION" 
+
 yum install -y libsndfile \
 libX11-devel \
 libXrandr-devel \
@@ -30,8 +36,8 @@ ldconfig
 pythonLibPath=/opt/python/cp$PYTHONMAJOR-cp$PYTHONMAJOR/lib
 pythonInclude=/opt/python/cp$PYTHONMAJOR-cp$PYTHONMAJOR/include/python$PYTHONVERSION
 
-echo "pythonLibPath: " $pythonLibPath
-echo "pythonInclude: " $pythonInclude
+echo "pythonLibPath: $pythonLibPath" 
+echo "pythonInclude: $pythonInclude" 
 
 PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig/:/usr/local/include/freetype2/freetype/config:../../alsa-lib/utils:/__w/DawDreamer/DawDreamer/alsa-lib/utils
 make VERBOSE=1 CONFIG=Release LIBS="-lstdc++fs" LDFLAGS="-L/__w/DawDreamer/DawDreamer/alsa-lib/src -L$pythonLibPath" CXXFLAGS="-I../../alsa-lib/include -I../../curl/include -I../../freetype/include -I$pythonInclude"
