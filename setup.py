5e5a8b4a16061ecdbd134fe4512e2cd6c6328961
David Braun <email@example.com>
2022-04-28 12:16:35 -0700

    --no-as-needed

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ elif platform.system() == "Linux":
             ['dawdreamer/null.c'],
             language='c++',
             libraries=['faust'],
+            extra_link_args=["-Wl,--no-as-needed"],
             library_dirs=[dawdreamer_dir, './', 'dawdreamer', './dawdreamer', '/usr/local/lib', '/usr/lib/x86_64-linux-gnu'],
             runtime_library_dirs=[dawdreamer_dir, './', 'dawdreamer', './dawdreamer', '/usr/local/lib', '/usr/lib/x86_64-linux-gnu'],
             data_files=[('/usr/lib/x86_64-linux-gnu', glob.glob(os.path.join(this_dir, 'dawdreamer/libfaust*.so*')))]
