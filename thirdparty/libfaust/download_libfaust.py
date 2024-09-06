4b761c2ca55df3cd025a0067f57e192098d057d0
David Braun <2096055+DBraun@users.noreply.github.com>
2024-09-06 15:23:15 -0400

    Update download_libfaust.py

--- thirdparty/libfaust/download_libfaust.py
+++ thirdparty/libfaust/download_libfaust.py
@@ -2,37 +2,43 @@ import argparse
 import os
 import platform
 import subprocess
+import sys
+import shutil
 from pathlib import Path
+from time import sleep
 
 
-def download_file(url: str, output: str) -> None:
+def download_file(url: str, output: str) -> bool:
+    """ Return True if a file was downloaded, False otherwise."""
     if os.path.exists(output) and not args.force:
         print(f"File already exists: {output}")
+        return False
     else:
         subprocess.run(["curl", "-L", url, "-o", output], check=True)
+        return True
 
 def install_windows(version: str) -> None:
     exe_file = f"Faust-{version}-win64.exe"
-    download_file(f"https://github.com/grame-cncm/faust/releases/download/{version}/{exe_file}", exe_file)
-    cwd = str(Path(__file__).parent)
-    subprocess.run([exe_file, "/S", f"/D={cwd}\\win64\\Release"], check=True)
+    if download_file(f"https://github.com/grame-cncm/faust/releases/download/{version}/{exe_file}", exe_file):
+        cwd = str(Path(__file__).parent)
+        subprocess.run([exe_file, "/S", f"/D={cwd}\\win64\\Release"], check=True)
 
 def install_macos(version: str) -> None:
     for arch in ["arm64", "x64"]:
         dmg_file = f"Faust-{version}-{arch}.dmg"
-        download_file(f"https://github.com/grame-cncm/faust/releases/download/{version}/{dmg_file}", dmg_file)
-        subprocess.run(["hdiutil", "attach", dmg_file], check=True)
-        dir_path = f"darwin-{arch}/Release"
-        os.makedirs(dir_path, exist_ok=True)
-        subprocess.run(["cp", "-R", f"/Volumes/Faust-{version}/Faust-{version}/*", dir_path], check=True)
-        subprocess.run(["hdiutil", "detach", f"/Volumes/Faust-{version}/"], check=True)
+        if download_file(f"https://github.com/grame-cncm/faust/releases/download/{version}/{dmg_file}", dmg_file):
+            subprocess.run(["hdiutil", "attach", dmg_file], check=True)
+            dir_path = f"darwin-{arch}/Release"
+            shutil.copytree(f"/Volumes/Faust-{version}/Faust-{version}", dir_path, dirs_exist_ok=True)
+            subprocess.run(["hdiutil", "detach", f"/Volumes/Faust-{version}/"], check=True)
+            sleep(1)  # this seems to prevent an issue where the second DMG is copied to both destinations
 
 def install_linux(version: str) -> None:
     zip_file = f"libfaust-ubuntu-x86_64.zip"
-    download_file(f"https://github.com/grame-cncm/faust/releases/download/{version}/{zip_file}", zip_file)
-    dir_path = "ubuntu-x86_64/Release"
-    os.makedirs(dir_path, exist_ok=True)
-    subprocess.run(["unzip", zip_file, "-d", dir_path], check=True)
+    if download_file(f"https://github.com/grame-cncm/faust/releases/download/{version}/{zip_file}", zip_file):
+        dir_path = "ubuntu-x86_64/Release"
+        os.makedirs(dir_path, exist_ok=True)
+        subprocess.run(["unzip", zip_file, "-d", dir_path], check=True)
 
 def main(version: str) -> None:
     system = platform.system()
@@ -47,6 +53,10 @@ def main(version: str) -> None:
 
 
 if __name__ == "__main__":
+    MIN_PYTHON = (3, 6)
+    if sys.version_info < MIN_PYTHON:
+        sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
+
     parser = argparse.ArgumentParser(description="Download and install Libfaust.")
     parser.add_argument("-v", "--version", default="2.74.6", help="Specify the version of Faust to download.")
     parser.add_argument("--force", action="store_true", help="Force download even if files already exist.")
