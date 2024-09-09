35b1719fc0d677abe3e06dfab143d948d67d955b
David Braun <2096055+DBraun@users.noreply.github.com>
2024-09-09 09:32:49 -0400

    fix sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ release = get_dawdreamer_version()
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -309,4 +309,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://user:password@docs.python.org/3', None)}
